db878e0b7a353d21d0900bd066ca9a4caf78ecd0
BNdungu <bryannganga909@gmail.com>
2023-06-11 08:33:50 +0300

    _

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql')
+require('dotenv').config()
 
 db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '*11Bryan!2002#',
-    database: 'login',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
 
 })
 db.connect((err) => {
